3742835502cf61923510aac7cb917f0100ff3395
miguel rovlich <mrovlich@enorchile.com>
2023-09-22 07:10:43 -0400

    CHG:[Home] mejorar la interfaz de diseno

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -10,7 +10,7 @@ function Navbar() {
 
   return (
     <nav className="navbar navbar-expand-lg navbar-white bg-white">
-      <div className="container-fluid">
+      <div className="container-md">
         {/* Logo a la izquierda */}
         <Link className="navbar-brand" to="/">
           <img src=".\src\assets\logo.jpeg" alt="" width="130" height="80" />
@@ -27,24 +27,24 @@ function Navbar() {
 
         {/* Menú de navegación */}
         <div className={`collapse navbar-collapse${expanded ? ' show' : ''}`}>
-          <ul className="navbar-nav  mb-2 mb-lg-0">
+          <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
             <li className="nav-item">
-              <Link className="nav-link" to="/" onClick={toggleNavbar}>
+              <Link className="nav-link text-center" to="/" onClick={toggleNavbar}>
                 Inicio
               </Link>
             </li>
             <li className="nav-item">
-              <Link className="nav-link" to="/nosotros" onClick={toggleNavbar}>
+              <Link className="nav-link  text-center" to="/nosotros" onClick={toggleNavbar}>
                 Nosotros
               </Link>
             </li>
             <li className="nav-item">
-              <Link className="nav-link" to="/nosotros" onClick={toggleNavbar}>
+              <Link className="nav-link  text-center" to="/nosotros" onClick={toggleNavbar}>
                 Servicios
               </Link>
             </li>
             <li className="nav-item">
-              <Link className="nav-link" to="/contacto" onClick={toggleNavbar}>
+              <Link className="nav-link btn btn-primary rounded-pill text-white  py-2  px-4" to="/contacto" onClick={toggleNavbar}>
                 Contacto
               </Link>
             </li>
--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -7,21 +7,18 @@ function Home() {
 
   return (
     <div className="container-fluid p-5">
-
         <div className="row">
-          <div className="col-md-6 px-5 py-4">
-            
-        <h1 className="mb-3">No buscamos ganarle a la competencia, crecemos junto a tu negocio</h1>
+          <div className="col-md-6 px-5 mt-5">
+        <h1 className="display-4 pt-3"><strong className="">No</strong> buscamos ganarle a la competencia</h1>
+        <h1 className="display-4"> <strong>Crecemos</strong> junto a tu negocio</h1>
         <p className="my-4">Tu agencia de marketing digital especializada en gestión de marcas.</p> 
-        <a className="btn btn-primary rounded-pill ">Escribir mi historia de exito</a>
+        <a className="btn btn-lg btn-primary rounded-pill ">Escribir mi historia de exito</a>
           </div>
-          
-          <div className="col-md-6 px-5">
-            <img src=".\src\assets\web-site-1706400_1280.png" alt="" width="330" height="340" />
+          <div className="col-md-6 d-flex justify-content-center align-items-center">
+            <img src=".\src\assets\web-site-1706400_1280.png" className='mx-5 my-5' alt="" width="450" height="450" />
           </div>
         </div>
     </div>
   )
 }
-
 export default Home
